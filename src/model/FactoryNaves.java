5f3ebd6d91a7c1a5695a9725495d9bdf4be5b5b9
Dlunaz <dcorrales002@ikasle.ehu.eus>
2026-04-05 14:11:53 +0200

    CrearJugador del Factory

---
+++ src/model/FactoryNaves.java
@@ -0,0 +1,39 @@
+package model;
+
+public class FactoryNaves {
+	
+	//Crea naves según el tipo
+	
+	private static FactoryNaves miFactory = null;
+	
+	private FactoryNaves() { }
+	
+	public static FactoryNaves getFactory() {
+		if (miFactory == null) {
+			miFactory = new FactoryNaves();
+		}
+		return miFactory;
+	}
+	
+	
+	//Crea un jugador según el tipo de nave que queramos
+	public Jugador crearJugador(String tipo) { 
+		
+		int x= 50;
+		int y=55;
+		if (tipo==null) {
+			return new Rojo(x,y); //Si no se especifica el tipo, por defecto es rojo
+		}
+		
+		switch (tipo) {
+			case "rojo":
+				return new Rojo(x,y);
+			case "azul":
+				return new Azul(x,y);	//TODO clase nave azul
+			case "verde":
+				return new Verde(x,y);	//TODO clase nave verde
+		
+		}
+	}
+
+}
