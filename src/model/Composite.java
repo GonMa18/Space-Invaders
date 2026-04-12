7b51dba9e03ab61afa900735495d833eb7e78b2f
Gonzalo <demagureguisanchezgonzalo@gmail.com>
2026-04-12 19:54:43 +0200

    limites jugador

--- src/model/Composite.java
+++ src/model/Composite.java
@@ -14,12 +14,16 @@ public class Composite implements Component {
     public void addPixel(Coordenada c) {
 	pixeles.add(c);
     }
-    public void mover(int x, int y) {
-        this.x += x;
-        this.y += y;
+    public void mover(int dx, int dy) {
         for (Coordenada c : this.pixeles) {
-            c.mover(x, y);
-            //System.out.println("moviendome a " + c.getX() + ", " + c.getY());
+            if (c.getX() + dx < 0 || c.getY() + dy < 0 || c.getX() + dx >= 160 || c.getY() + dy >= 120) {
+                return; // No mover si alguno de los pixeles se saldría del espacio
+            }
+        }
+        this.x += dx;
+        this.y += dy;
+        for (Coordenada c : this.pixeles) {
+            c.mover(dx, dy);
         }
     }
     public boolean containPixel(int x, int y) {
